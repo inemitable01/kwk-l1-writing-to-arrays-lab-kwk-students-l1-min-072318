342f6617373c1eed4e01db15eb0c74b0f64f86d1
barnesemily12@gmail.com <barnesemily12@gmail.com>
2018-07-26 01:22:36 +0000

    automatically backed up by learn

--- rain.rb
+++ rain.rb
@@ -1,6 +1,12 @@
-rainbow_colors = [
+def change_rainbow_colors
+  rainbow_colors = [
   "yellow",
   "default",
   "light_cyan"
 ]
-print rainbow_colors
+ rainbow_colors[0]= "red"
+ rainbow_colors[1]= "light_red"
+ rainbow_colors[2]= "light_yellow"
+end
+ change_rainbow_colors
+ 
